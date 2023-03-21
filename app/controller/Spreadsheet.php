f32614906fb98db27449bab815569228034afa95
Aulia Ridho <auliaridho51@gmail.com>
2023-03-21 15:58:17 +0700

    Fetch all devices using repository

--- app/controller/Spreadsheet.php
+++ app/controller/Spreadsheet.php
@@ -1,15 +1,30 @@
 <?php
 namespace App\Controller;
 
+use App\Core\ConnectDB;
+use App\Core\Database\MysqliAdapter;
 use App\View\View;
+use App\Model\Repository\Transaction\Repo;
+use App\Model\Repository\Device\DeviceRepo;
 
 class SpreadsheetController
 {
 
     public function index()
     {
-        $params=['data'];
+        $params=['data' => [
+            'devices' => ''
+        ]];
         $view = new View('spreadsheet/index', $params);
         return $view->render();
+    }
+
+    public function getDeviceList()
+    {
+        $adapter = new MysqliAdapter(new ConnectDB);
+        $deviceRepo = new DeviceRepo($adapter);
+        
+        $res = $deviceRepo->fetchAll();
+        return $res;
     }
 }
--- app/model/Repository/Device/Device.php
+++ app/model/Repository/Device/Device.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Repository\Device;
 
+use App\Core\ConnectDB;
 use App\Core\Database\AdapterInterface;
 use App\Model\Device;
 use App\Model\Mapper\Device\DeviceMapper;
@@ -8,12 +9,53 @@ use App\Model\Mapper\Device\DeviceMapper;
 class DeviceRepo
 {
     private $mapper;
+
+    /**
+     * Don't use it
+     */
+    private $adapter;
+
+    /**
+     * Database connection
+     */
     private $db;
 
+    /**
+     * Creates adapter, mapper, and db connection
+     */
     public function __construct(AdapterInterface $adapter)
     {
-        $this->db = $adapter;
-        $this->mapper = new DeviceMapper($this->db);
+        $this->adapter = $adapter;
+        $this->mapper = new DeviceMapper($this->adapter);
+        $this->db = (new ConnectDB())->connectTo();
+    }
+
+    /**
+     * Get list of all devices from LAN and WAN category;
+     * 
+     * @return string | return list of LAN and WAN devices in JSON {data: [LAN: [{idDevice: "...", nameDevice: "...", nameCategory: "..."}, ...], WAN: [{idDevice: "...", nameDevice: "...", nameCategory: "..."}, ...]}
+     */
+    public function fetchAll()
+    {
+        $db = $this->db;
+        $queryLan = "SELECT idDevice, nameDevice, nameCategory FROM device LEFT JOIN category ON device.idCategory = category.idCategory WHERE device.idCategory = '2325eesc' GROUP BY idDevice";
+        $queryWan = "SELECT idDevice, nameDevice, nameCategory FROM device LEFT JOIN category ON device.idCategory = category.idCategory WHERE device.idCategory = '2ae27792' GROUP BY idDevice";
+
+        $lan = $db->prepare($queryLan);
+        $lan->execute();
+        $lanResult = $lan->get_result()->fetch_all(MYSQLI_ASSOC);
+
+        $wan = $db->prepare($queryWan);
+        $wan->execute();
+        $wanResult = $wan->get_result()->fetch_all(MYSQLI_ASSOC);
+
+        $data = [
+            'LAN' => $lanResult,
+            'WAN' => $wanResult
+        ];
+        
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode(array('data' => $data));
     }
 
     public function create(String $idDevice)
