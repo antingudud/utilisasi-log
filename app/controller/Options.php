c7a2bed3abc24e995eb62a74417261a199be5d7b
Aulia Ridho <68772295+antingudud@users.noreply.github.com>
2023-03-31 13:29:01 +0700

    Purge unused files (#5)
    * Purge unused files and codes
    
    * Better error handling

--- app/controller/Options.php
+++ app/controller/Options.php
@@ -9,22 +9,4 @@ class OptionsContr
     {
         return (new Options($_POST))->getDevices();
     }
-
-    public function showEditList()
-    {
-        $errors = [];
-        $id = $_POST;
-        foreach ($id['id'] as $key => $value) {
-            if (empty($value) || strlen($value) > 8 || !preg_match('/^[a-zA-Z0-9]+$/', $value)) {
-                $errors = ['error' => 'Invalid device ID'];
-            }
-        }
-        if (!empty($errors)) {
-            header('HTTP/1.1 400 Bad Request');
-            echo json_encode(['error' => 'Invalid device ID']);
-            return;
-        }
-
-        echo json_encode( (new DeviceService)->getEditList($id) );
-    }
 }
