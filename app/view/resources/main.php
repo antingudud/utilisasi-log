c84a2d9c47bb9f50c90ce3c668ada362bef79c06
Aulia Ridho <auliaridho51@gmail.com>
2023-03-31 13:08:21 +0700

    minor tweaks

--- app/view/resources/main.php
+++ app/view/resources/main.php
@@ -13,8 +13,8 @@
     <title>Home</title>
 </head>
 
-<body>
-    <nav class="sticky top-0 z-50 flex items-center h-24 w-screen min-h-full max-h-28 bg-slate-900">
+<body class="h-screen">
+    <nav class="sticky top-0 z-50 flex items-center h-24 w-screen max-h-28 bg-slate-900">
         <div class="flex justify-between w-full mr-16 ml-16">
             <ul class="flex list-none m-0">
                 <li class="mr-6">
@@ -32,7 +32,7 @@
         </div>
     </nav>
 
-    <section id="mainbody" class="container w-screen h-full pr-6 pl-6 pt-6 pb-6 items-center justify-center">
+    <section id="mainbody" class="container relative w-screen h-screen pr-6 pl-6 pt-6 pb-6 items-center justify-center">
 
         {{content}}
 
